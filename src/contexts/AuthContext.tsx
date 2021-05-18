1f619bc1542afc4eef1a357af1ce1b5f35c6e498
Lucas Macedo <lucasmdc57@gmail.com>
2021-05-18 08:41:55 -0300

    logout with broadcast channel

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -15,7 +15,8 @@ type SignInCredentials = {
 }
 
 type AuthContextData = {
-  signIn(credentials: SignInCredentials): Promise<void>;
+  signIn: (credentials: SignInCredentials) => Promise<void>;
+  signOut: () => void;
   user: User;
   isAuthenticated: boolean;
 }
@@ -26,16 +27,34 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData)
 
+let authChannel: BroadcastChannel
+
 export function signOut() {
   destroyCookie(undefined, 'nextauth.token')
   destroyCookie(undefined, 'nextauth.refreshToken')
 
+  authChannel.postMessage('signOut')
+  
   Router.push('/')
 }
 
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>(null)
   const isAuthenticated = !!user
+
+  useEffect(() => {
+    authChannel = new BroadcastChannel('auth')
+    
+    authChannel.onmessage = (message) => {
+      switch (message.data) {
+        case 'signOut':
+          signOut();
+          break;
+        default:
+          break
+      }
+    }
+  }, [])
 
   useEffect(() => {
     const { 'nextauth.token': token } = parseCookies()
@@ -79,7 +98,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user, signOut }}>
       { children }
     </AuthContext.Provider>
   )
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -5,22 +5,10 @@ import { withSSRAuth } from "../utils/withSSRAuth"
 import Head from 'next/head'
 
 import styles from '../styles/Home.module.css'
-import { api } from "../services/apiClient"
-import { useCan } from "../hooks/useCan"
 import { Can } from "../components/Can"
 
 function Dashboard () {
-  const { user } = useContext(AuthContext)
-
-  const userCanSeeMetrics = useCan({
-    permissions: ['metrics.list']
-  })
-
-  useEffect(() => {
-    api.get('/me')
-      .then(response => console.log(response))
-      .catch(error => console.log(error))
-  })
+  const { user, signOut } = useContext(AuthContext)
 
   return (
     <div className={styles.container}>
@@ -29,6 +17,7 @@ function Dashboard () {
       </Head>
 
       <h1>Dashboard for: { user?.email }</h1>
+      <button onClick={signOut}>Sign Out</button>
 
       <Can permissions={['metrics.list']}>
         <div>
